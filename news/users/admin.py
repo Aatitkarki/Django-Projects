0dfe83a0592a49dfafa97985cbfab33527827b04
Suresh <aatitkarki123@gmail.com>
2020-09-02 16:42:56 +0545

    latest

--- news/users/admin.py
+++ news/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm,CustomUserCreationForm
 
-# Register your models here.
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+admin.site.register(CustomUser,CustomUserAdmin)
